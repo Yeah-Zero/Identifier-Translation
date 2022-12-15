7acefee78d2ffd24a0f8258b0e24cc9fde0c5c01
烨凌 <92421089+Yeah-Zero@users.noreply.github.com>
2022-12-15 23:48:25 +0800

    提交

--- src/main/java/Yeah_Zero/Namespaced_Identifier_Translation/Mixin/LocateCommandMixin.java
+++ src/main/java/Yeah_Zero/Namespaced_Identifier_Translation/Mixin/LocateCommandMixin.java
@@ -18,6 +18,9 @@ import java.time.Duration;
 
 @Mixin(LocateCommand.class)
 public class LocateCommandMixin {
+    private static String 标签;
+    private static String 标识符;
+
     private static String 获取键名字符串(Pair<BlockPos, ? extends RegistryEntry<?>> 结果) {
         return 结果.getSecond().getKey().map((键名) -> {
             return 键名.getValue().toString();
@@ -26,16 +29,32 @@ public class LocateCommandMixin {
 
     @Inject(method = "sendCoordinates(Lnet/minecraft/server/command/ServerCommandSource;Lnet/minecraft/command/argument/RegistryEntryPredicateArgumentType$EntryPredicate;Lnet/minecraft/util/math/BlockPos;Lcom/mojang/datafixers/util/Pair;Ljava/lang/String;ZLjava/time/Duration;)I", at = @At("HEAD"))
     private static void 获取谓词(ServerCommandSource 来源, RegistryEntryPredicateArgumentType.EntryPredicate<?> 谓词, BlockPos 当前坐标, Pair<BlockPos, ? extends RegistryEntry<?>> 结果, String 成功消息, boolean 包括Y坐标, Duration 用时, CallbackInfoReturnable<Integer> 可返回回调信息) {
-        System.out.println(获取键名字符串(结果));
+        谓词.getEntry().map((条目) -> {
+            标识符 = 谓词.asString();
+            return null;
+        }, (这是标签哦) -> {
+            标签 = 谓词.asString();
+            标识符 = 获取键名字符串(结果);
+            return null;
+        });
     }
 
     @Inject(method = "sendCoordinates(Lnet/minecraft/server/command/ServerCommandSource;Lnet/minecraft/command/argument/RegistryPredicateArgumentType$RegistryPredicate;Lnet/minecraft/util/math/BlockPos;Lcom/mojang/datafixers/util/Pair;Ljava/lang/String;ZLjava/time/Duration;)I", at = @At("HEAD"))
     private static void 获取结构(ServerCommandSource 来源, RegistryPredicateArgumentType.RegistryPredicate<?> 结构, BlockPos 当前坐标, Pair<BlockPos, ? extends RegistryEntry<?>> 结果, String 成功消息, boolean 包括Y坐标, Duration 用时, CallbackInfoReturnable<Integer> 可返回回调信息) {
-        System.out.println(获取键名字符串(结果));
+        结构.getKey().map((键名) -> {
+            标识符 = 键名.getValue().toString();
+            return null;
+        }, (键名) -> {
+            标签 = 键名.id().toString();
+            标识符 = 获取键名字符串(结果);
+            return null;
+        });
     }
 
     @Redirect(method = "sendCoordinates(Lnet/minecraft/server/command/ServerCommandSource;Lnet/minecraft/util/math/BlockPos;Lcom/mojang/datafixers/util/Pair;Ljava/lang/String;ZLjava/lang/String;Ljava/time/Duration;)I", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/command/ServerCommandSource;sendFeedback(Lnet/minecraft/text/Text;Z)V"))
     private static void 发送坐标重定向(ServerCommandSource 来源, Text 消息, boolean 广播给管理员) {
+        标签 = null;
+        标识符 = null;
         来源.sendFeedback(消息, 广播给管理员);
     }
 }
