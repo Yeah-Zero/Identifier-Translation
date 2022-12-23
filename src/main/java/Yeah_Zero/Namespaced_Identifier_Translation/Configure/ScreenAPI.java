e5207462c8ff79bc53ed6b0e46cad824e5b4f1ed
烨凌 <92421089+Yeah-Zero@users.noreply.github.com>
2022-12-23 11:44:11 +0800

    提交

--- src/main/java/Yeah_Zero/Namespaced_Identifier_Translation/Configure/ScreenAPI.java
+++ src/main/java/Yeah_Zero/Namespaced_Identifier_Translation/Configure/ScreenAPI.java
@@ -1,4 +1,8 @@
 package Yeah_Zero.Namespaced_Identifier_Translation.Configure;
 
-public class ScreenAPI {
+public class ScreenAPI implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return Configuration::屏幕;
+    }
 }
