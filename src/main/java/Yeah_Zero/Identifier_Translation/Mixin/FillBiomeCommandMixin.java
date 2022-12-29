bb90255e21a8b9c6d73c375b8057c28d1c284df9
烨凌 <92421089+Yeah-Zero@users.noreply.github.com>
2022-12-29 22:23:50 +0800

    提交

--- src/main/java/Yeah_Zero/Identifier_Translation/Mixin/FillBiomeCommandMixin.java
+++ src/main/java/Yeah_Zero/Identifier_Translation/Mixin/FillBiomeCommandMixin.java
@@ -1,8 +1,50 @@
 package Yeah_Zero.Identifier_Translation.Mixin;
 
+import Yeah_Zero.Identifier_Translation.Translator;
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
+import com.mojang.brigadier.suggestion.Suggestions;
+import net.minecraft.client.network.ClientCommandSource;
+import net.minecraft.command.CommandSource;
+import net.minecraft.command.suggestion.SuggestionProviders;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.FillBiomeCommand;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Style;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.Locale;
 
 @Mixin(FillBiomeCommand.class)
 public class FillBiomeCommandMixin {
+    private static final SuggestionProvider<ServerCommandSource> 具体生物群系建议提供者 = SuggestionProviders.register(new Identifier("specific_biome"), (命令内容, 建议构建器) -> {
+        return 命令内容.getSource().getRegistryManager().getOptional(RegistryKeys.BIOME).map((注册项) -> {
+            CommandSource.forEachMatching(注册项.getIds(), 建议构建器.getRemaining().toLowerCase(Locale.ROOT), (标识符) -> {
+                return 标识符;
+            }, (标识符) -> {
+                建议构建器.suggest(标识符.toString(), Translator.翻译("biome", 标识符.toString()).copy().setStyle(Style.EMPTY));
+            });
+            return 建议构建器.buildFuture();
+        }).orElseGet(() -> {
+            if (命令内容.getSource() instanceof ClientCommandSource) {
+                return 命令内容.getSource().getCompletions(命令内容);
+            } else {
+                return Suggestions.empty();
+            }
+        });
+    });
+
+    @Redirect(method = "register(Lcom/mojang/brigadier/CommandDispatcher;Lnet/minecraft/command/CommandRegistryAccess;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/command/CommandManager;argument(Ljava/lang/String;Lcom/mojang/brigadier/arguments/ArgumentType;)Lcom/mojang/brigadier/builder/RequiredArgumentBuilder;"))
+    private static <T> RequiredArgumentBuilder<ServerCommandSource, T> 提供结构建议(String 名称, ArgumentType<T> 类型) {
+        return switch (名称) {
+            case "biome" -> CommandManager.argument(名称, 类型).suggests(具体生物群系建议提供者);
+            case "filter" -> CommandManager.argument(名称, 类型).suggests(Translator.生物群系建议提供者);
+            default -> CommandManager.argument(名称, 类型);
+        };
+    }
 }
--- src/main/java/Yeah_Zero/Identifier_Translation/Translator.java
+++ src/main/java/Yeah_Zero/Identifier_Translation/Translator.java
@@ -1,19 +1,60 @@
 package Yeah_Zero.Identifier_Translation;
 
 import Yeah_Zero.Identifier_Translation.Configure.Configuration;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
+import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.datafixers.util.Pair;
+import net.minecraft.client.network.ClientCommandSource;
+import net.minecraft.command.CommandSource;
+import net.minecraft.command.suggestion.SuggestionProviders;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.HoverEvent;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Util;
 import net.minecraft.util.math.BlockPos;
 
+import java.util.Locale;
+
 public class Translator {
+    public static final SuggestionProvider<ServerCommandSource> 结构建议提供者 = Translator.建议提供("structure", RegistryKeys.STRUCTURE);
+    public static final SuggestionProvider<ServerCommandSource> 生物群系建议提供者 = Translator.建议提供("biome", RegistryKeys.BIOME);
+    public static final SuggestionProvider<ServerCommandSource> 兴趣点建议提供者 = Translator.建议提供("poi", RegistryKeys.POINT_OF_INTEREST_TYPE);
+
     public static String 获取键名字符串(Pair<BlockPos, ? extends RegistryEntry<?>> 结果) {
         return 结果.getSecond().getKey().map((键名) -> {
             return 键名.getValue().toString();
         }).orElse(Text.translatable("[unregistered]").getString());
+    }
+
+    private static <T> SuggestionProvider<ServerCommandSource> 建议提供(String 类型, RegistryKey<Registry<T>> 注册项键) {
+        return SuggestionProviders.register(new Identifier(类型), (命令内容, 建议构建器) -> {
+            return 命令内容.getSource().getRegistryManager().getOptional(注册项键).map((注册项) -> {
+                CommandSource.forEachMatching(注册项.streamTags().map(TagKey::id)::iterator, 建议构建器.getRemaining().toLowerCase(Locale.ROOT), "#", (标识符) -> {
+                    return 标识符;
+                }, (标识符) -> {
+                    建议构建器.suggest("#" + 标识符, Text.translatable(Util.createTranslationKey("tag", 标识符)));
+                });
+                CommandSource.forEachMatching(注册项.getIds(), 建议构建器.getRemaining().toLowerCase(Locale.ROOT), (标识符) -> {
+                    return 标识符;
+                }, (标识符) -> {
+                    建议构建器.suggest(标识符.toString(), 翻译(类型, 标识符.toString()).copy().setStyle(Style.EMPTY));
+                });
+                return 建议构建器.buildFuture();
+            }).orElseGet(() -> {
+                if (命令内容.getSource() instanceof ClientCommandSource) {
+                    return 命令内容.getSource().getCompletions(命令内容);
+                } else {
+                    return Suggestions.empty();
+                }
+            });
+        });
     }
 
     private static Text 生物群系名称附加(String 标识符翻译键名) {
@@ -29,24 +70,24 @@ public class Translator {
         };
     }
 
-    public static Text 翻译(String 类型, String 标识符) {
-        if (标识符.startsWith("#")) {
-            return 翻译("tag", 标识符.substring(1));
+    public static Text 翻译(String 类型, String 标识符字符串) {
+        if (标识符字符串.startsWith("#")) {
+            return 翻译("tag", 标识符字符串.substring(1));
         } else {
-            String 标识符翻译键名 = Util.createTranslationKey(类型, new Identifier(标识符));
+            String 标识符翻译键名 = Util.createTranslationKey(类型, new Identifier(标识符字符串));
             if (类型.equals("poi")) {
                 标识符翻译键名 = Text.translatable(标识符翻译键名).getString();
             }
             if (类型.equals("tag")) {
-                return Text.literal("#" + 标识符).setStyle(Configuration.配置项.标签样式.生成样式().withHoverEvent(Configuration.配置项.标签样式.显示悬停文本 ? new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.translatable(标识符翻译键名)) : null));
+                return Text.literal("#" + 标识符字符串).setStyle(Configuration.配置项.标签样式.生成样式().withHoverEvent(Configuration.配置项.标签样式.显示悬停文本 ? new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.translatable(标识符翻译键名)) : null));
             } else {
                 String 标识符翻译;
-                if ((标识符.contains("mineshaft") && !标识符.equals("minecraft:mineshaft")) || 标识符.contains("ocean_ruin") || (标识符.contains("ruined_portal") && !标识符.equals("minecraft:ruined_portal")) || (标识符.contains("shipwreck") && !标识符.equals("minecraft:shipwreck")) || 标识符.contains("village")) {
+                if ((标识符字符串.contains("mineshaft") && !标识符字符串.equals("minecraft:mineshaft")) || 标识符字符串.contains("ocean_ruin") || (标识符字符串.contains("ruined_portal") && !标识符字符串.equals("minecraft:ruined_portal")) || (标识符字符串.contains("shipwreck") && !标识符字符串.equals("minecraft:shipwreck")) || 标识符字符串.contains("village")) {
                     标识符翻译 = Text.translatable("%s (%s)", Text.translatable(标识符翻译键名), 生物群系名称附加(标识符翻译键名)).getString();
                 } else {
                     标识符翻译 = Text.translatable(标识符翻译键名).getString();
                 }
-                return Text.literal(标识符翻译).setStyle(Configuration.配置项.标识符样式.生成样式().withHoverEvent(Configuration.配置项.标识符样式.显示悬停文本 ? new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal(标识符)) : null));
+                return Text.literal(标识符翻译).setStyle(Configuration.配置项.标识符样式.生成样式().withHoverEvent(Configuration.配置项.标识符样式.显示悬停文本 ? new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal(标识符字符串)) : null));
             }
         }
     }
