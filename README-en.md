fc8f3817e1f63b848a7ef3aa89c4c717230e4206
烨凌 <92421089+Yeah-Zero@users.noreply.github.com>
2022-12-31 14:59:21 +0800

    提交

--- README-en.md
+++ README-en.md
@@ -1,1 +1,50 @@
+<p align="right">
+	<a href="./README-en.md"><b>中文版</b></a>
+</p>
 
+<p align="left">
+	<img src="./src/main/resources/assets/identifier-translation/icon.png" />
+</p>
+
+<h1 align="center">
+	Identifier Translation
+</h1> 
+
+<p align="center">
+	将命令反馈与命令建议中的生物群系、地物和结构显示为对应翻译而非标识符。
+</p>
+
+<p align="center">
+	<a href="./LICENSE">
+		<img src="https://img.shields.io/github/license/Yeah-Zero/Identifier-Translation?label=License&style=flat-square" /></a>
+	<img src="https://img.shields.io/github/languages/code-size/Yeah-Zero/Identifier-Translation?label=Size&style=flat-square" />
+	<img src="https://img.shields.io/static/v1?label=Loader&message=Fabric | Quilt&color=brightgreen&style=flat-square" />
+</p>
+
+<p align="center">
+    <img src="./演示图片/效果演示1.png" />
+    <img src="./演示图片/效果演示2.png" />
+    <img src="./演示图片/效果演示3.png" />
+</p>
+
+## Links
+
+[GitHub](https://github.com/Yeah-Zero/Identifier-Translation) [CurseForge](https://www.curseforge.com/minecraft/mc-mods/identifier-translation) [Modrinth](https://modrinth.com/mod/identifier-translation) [MCBBS]() [MCmod](https://www.mcmod.cn/class/XXXX.html)
+
+### Dependencies
+
+Cloth
+Config ([GitHub](https://github.com/shedaniel/cloth-config) [CurseForge](https://www.curseforge.com/minecraft/mc-mods/cloth-config) [Modrinth](https://modrinth.com/mod/cloth-config))
+
+Fabric
+API ([GitHub](https://github.com/FabricMC/fabric) [CurseForge](https://www.curseforge.com/minecraft/mc-mods/fabric-api) [Modrinth](https://modrinth.com/mod/fabric-api))
+
+Fabric Loader ([Official Website](https://fabricmc.net/) [GitHub](https://github.com/FabricMC/fabric-loader))
+
+### Instruction
+
+生物群系翻译取自游戏内语言文件，地物（仅中文）和结构翻译取自[Minecraft Wiki](https://minecraft.fandom.com/wiki/Minecraft_Wiki)。
+
+涉及命令：`/fillbiome`、`/locate`和`/place`。
+
+Configuration file is saved at "\config\标识符翻译.json".
--- src/main/java/Yeah_Zero/Identifier_Translation/Translator.java
+++ src/main/java/Yeah_Zero/Identifier_Translation/Translator.java
@@ -4,6 +4,7 @@ import Yeah_Zero.Identifier_Translation.Configure.Configuration;
 import com.mojang.brigadier.suggestion.SuggestionProvider;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.datafixers.util.Pair;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientCommandSource;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.suggestion.SuggestionProviders;
@@ -85,13 +86,10 @@ public class Translator {
         if (标识符字符串.startsWith("#")) {
             return 翻译("tag", 标识符字符串.substring(1));
         } else {
-            String 标识符翻译键名 = Util.createTranslationKey(类型, new Identifier(标识符字符串));
-            if (类型.equals("poi")) {
-                标识符翻译键名 = Text.translatable(标识符翻译键名).getString();
-            }
+            String 标识符翻译键名 = Text.translatable(Util.createTranslationKey(类型, new Identifier(标识符字符串))).getString();
             if (类型.equals("tag")) {
                 return Text.literal("#" + 标识符字符串).setStyle(Configuration.配置项.标签样式.生成样式().withHoverEvent(Configuration.配置项.标签样式.显示悬停文本 ? new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.translatable(标识符翻译键名)) : null));
-            } else if (类型.equals("configured_feature")) {
+            } else if (类型.equals("configured_feature") && !MinecraftClient.getInstance().options.language.contains("zh")) {
                 return Text.literal(标识符字符串).setStyle(Configuration.配置项.标识符样式.生成样式().withHoverEvent(Configuration.配置项.标识符样式.显示悬停文本 ? new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.translatable(标识符翻译键名)) : null));
             } else {
                 String 标识符翻译;
@@ -105,18 +103,15 @@ public class Translator {
         }
     }
 
-    public static Text 翻译(String 类型, String 标签, String 标识符) {
-        String 标签描述键名 = Util.createTranslationKey("tag", new Identifier(标签.substring(1)));
-        String 标识符翻译键名 = Util.createTranslationKey(类型, new Identifier(标识符));
-        if (类型.equals("poi")) {
-            标识符翻译键名 = Text.translatable(标识符翻译键名).getString();
-        }
+    public static Text 翻译(String 类型, String 标签字符串, String 标识符字符串) {
+        String 标签描述键名 = Util.createTranslationKey("tag", new Identifier(标签字符串.substring(1)));
+        String 标识符翻译键名 = Text.translatable(Util.createTranslationKey(类型, new Identifier(标识符字符串))).getString();
         String 标识符翻译;
-        if ((标识符.contains("mineshaft") && !标识符.equals("minecraft:mineshaft")) || 标识符.contains("ocean_ruin") || (标识符.contains("ruined_portal") && !标识符.equals("minecraft:ruined_portal")) || (标识符.contains("shipwreck") && !标识符.equals("minecraft:shipwreck")) || 标识符.contains("village")) {
+        if ((标识符字符串.contains("mineshaft") && !标识符字符串.equals("minecraft:mineshaft")) || 标识符字符串.contains("ocean_ruin") || (标识符字符串.contains("ruined_portal") && !标识符字符串.equals("minecraft:ruined_portal")) || (标识符字符串.contains("shipwreck") && !标识符字符串.equals("minecraft:shipwreck")) || 标识符字符串.contains("village")) {
             标识符翻译 = Text.translatable("%s (%s)", Text.translatable(标识符翻译键名), 生物群系名称附加(标识符翻译键名)).getString();
         } else {
             标识符翻译 = Text.translatable(标识符翻译键名).getString();
         }
-        return Text.translatable("%s (%s)", Text.literal(标签).setStyle(Configuration.配置项.标签样式.生成样式().withHoverEvent(Configuration.配置项.标签样式.显示悬停文本 ? new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.translatable(标签描述键名)) : null)), Text.literal(标识符翻译).setStyle(Configuration.配置项.标识符样式.生成样式().withHoverEvent(Configuration.配置项.标识符样式.显示悬停文本 ? new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal(标识符)) : null)));
+        return Text.translatable("%s (%s)", Text.literal(标签字符串).setStyle(Configuration.配置项.标签样式.生成样式().withHoverEvent(Configuration.配置项.标签样式.显示悬停文本 ? new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.translatable(标签描述键名)) : null)), Text.literal(标识符翻译).setStyle(Configuration.配置项.标识符样式.生成样式().withHoverEvent(Configuration.配置项.标识符样式.显示悬停文本 ? new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.literal(标识符字符串)) : null)));
     }
 }
