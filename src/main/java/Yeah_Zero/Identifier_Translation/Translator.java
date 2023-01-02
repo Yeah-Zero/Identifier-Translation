37e3dcd055fa4f0b8240cc0d482e6cd343430629
烨凌 <92421089+Yeah-Zero@users.noreply.github.com>
2023-01-02 17:57:36 +0800

    提交

--- src/main/java/Yeah_Zero/Identifier_Translation/Translator.java
+++ src/main/java/Yeah_Zero/Identifier_Translation/Translator.java
@@ -88,7 +88,10 @@ public class Translator {
         if (标识符字符串.startsWith("#")) {
             return 翻译("tag", 标识符字符串.substring(1));
         } else {
-            String 标识符翻译键名 = Text.translatable(Util.createTranslationKey(类型, new Identifier(标识符字符串))).getString();
+            String 标识符翻译键名 = Util.createTranslationKey(类型, new Identifier(标识符字符串));
+            if (类型.equals("poi")) {
+                标识符翻译键名 = Text.translatable(标识符翻译键名).getString();
+            }
             if (类型.equals("tag")) {
                 return Text.literal("#" + 标识符字符串).setStyle(Configuration.配置项.标签样式.生成样式().withHoverEvent(Configuration.配置项.标签样式.显示悬停文本 ? new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.translatable(标识符翻译键名)) : null));
             } else if (类型.equals("configured_feature") && !MinecraftClient.getInstance().options.language.contains("zh")) {
@@ -107,7 +110,10 @@ public class Translator {
 
     public static Text 翻译(String 类型, String 标签字符串, String 标识符字符串) {
         String 标签描述键名 = Util.createTranslationKey("tag", new Identifier(标签字符串.substring(1)));
-        String 标识符翻译键名 = Text.translatable(Util.createTranslationKey(类型, new Identifier(标识符字符串))).getString();
+        String 标识符翻译键名 = Util.createTranslationKey(类型, new Identifier(标识符字符串));
+        if (类型.equals("poi")) {
+            标识符翻译键名 = Text.translatable(标识符翻译键名).getString();
+        }
         String 标识符翻译;
         if ((标识符字符串.contains("mineshaft") && !标识符字符串.equals("minecraft:mineshaft")) || 标识符字符串.contains("ocean_ruin") || (标识符字符串.contains("ruined_portal") && !标识符字符串.equals("minecraft:ruined_portal")) || (标识符字符串.contains("shipwreck") && !标识符字符串.equals("minecraft:shipwreck")) || 标识符字符串.contains("village")) {
             标识符翻译 = Text.translatable("%s (%s)", Text.translatable(标识符翻译键名), 生物群系名称附加(标识符翻译键名)).getString();
